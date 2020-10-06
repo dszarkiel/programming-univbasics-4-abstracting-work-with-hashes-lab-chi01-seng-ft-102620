40abcfe23a96ea67d6bb1812f92374e690df37f7
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-06 15:22:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def my_hash_creator(key, value)
-  new_hash = {key => value}
+  hash = {key => value}
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  hash =
 end
 
 def update_counting_hash(hash, key)
